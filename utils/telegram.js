c5787cb38591e6e6eaf73d820555cd7006712dd7
Khalidops <SaudiWrap@gmail.com>
2025-06-29 02:53:33 +0300

    Update telegram.js

--- utils/telegram.js
+++ utils/telegram.js
@@ -1,1 +1,13 @@
+const TelegramBot = require('node-telegram-bot-api');
+const { botToken } = require('../config');
 
+const bot = new TelegramBot(botToken, { polling: false });
+
+const sendMessage = (chatId, text) => {
+  return bot.sendMessage(chatId, text, { parse_mode: 'Markdown' });
+};
+
+module.exports = {
+  sendMessage,
+  bot,
+};
