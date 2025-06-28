576fe7dfa9d5919ed5c9e2de1e6457a202139479
Khalidops <SaudiWrap@gmail.com>
2025-06-29 02:54:33 +0300

    Update helper.js

--- utils/helper.js
+++ utils/helper.js
@@ -1,1 +1,10 @@
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
+const formatDate = (date) => {
+  return date.toISOString().replace('T', ' ').split('.')[0];
+};
+
+module.exports = {
+  delay,
+  formatDate,
+};
