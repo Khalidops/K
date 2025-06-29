c13b2974fa5289e3b4072f18ba416b138be20ec9
Khalidops <SaudiWrap@gmail.com>
2025-06-29 04:07:32 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,7 @@ module.exports = {
   botToken: '7640768152:AAEOncgFf7Ruk7VsSm4bJUF4XYeS4YX4qmw',
   chatId: '1323327820',
   lang: 'ar',
-  soundAlert: true
+  soundAlert: true,
+  // قائمة الفريمات الزمنية المدعومة للبوت (بالدقائق)
+  timeFrames: [1, 5, 10]
 };
