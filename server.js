ce6394fce3bc910c97353c43a0a95504111216e2
Khalidops <SaudiWrap@gmail.com>
2025-06-29 03:24:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
 
+app.get('/', (req, res) => {
+  res.send('🤖 بوت التداول يعمل حالياً.');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+});
