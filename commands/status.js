3972b513246bcc36b3bf3756898b1fd4c1b12f32
Khalidops <SaudiWrap@gmail.com>
2025-06-29 03:02:32 +0300

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -1,1 +1,7 @@
-
+module.exports = (bot, msg, status) => {
+  const chatId = msg.chat.id;
+  const text = status
+    ? 'البوت يعمل حالياً.'
+    : 'البوت متوقف حالياً.';
+  bot.sendMessage(chatId, text);
+};
