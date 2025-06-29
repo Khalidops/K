8c2a63d314bd40f85cf17af3ce7f248bcafc5de1
Khalidops <SaudiWrap@gmail.com>
2025-06-29 03:03:22 +0300

    Update settings.js

--- commands/settings.js
+++ commands/settings.js
@@ -1,1 +1,18 @@
+module.exports = (bot, msg, settings) => {
+  const chatId = msg.chat.id;
+  let text = 'إعدادات البوت الحالية:\n';
 
+  if (settings.soundAlert) {
+    text += '- التنبيه الصوتي مفعل\n';
+  } else {
+    text += '- التنبيه الصوتي معطل\n';
+  }
+
+  if (settings.lang === 'ar') {
+    text += '- اللغة: العربية\n';
+  } else {
+    text += '- اللغة: غير محددة\n';
+  }
+
+  bot.sendMessage(chatId, text);
+};
